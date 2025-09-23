3376528d13c331615e32f1242908e496d581a9f7
alexandra767 <alexandratitus767@gmail.com>
2025-09-23 05:08:45 -0400

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("hello Alex")
+
+var i = 0
+while i < 10 {
+    print("hello \(greeting)")
+    i += 1
+}
+
+for i in 0..<10 {
+    print(greeting)
+}
+
+let myFirst:  Date = Date()
+
+var userIsPremium: Bool = false
+
+
+
