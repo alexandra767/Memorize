c14b25555b23cdd11d5f9612f81736a79ea14758
alexandra767 <alexandratitus767@gmail.com>
2025-09-26 08:05:11 -0400

    circle random c olor when clicked

--- Memorize/Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/Memorize/ContentView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var circleColor: Color = .blue
+
+    private func randomColor() -> Color {
+        let red = Double.random(in: 0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(red: red, green: green, blue: blue)
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Circle()
+                    .fill(circleColor)
+                    .frame(width: 80, height: 80)
+                    .onTapGesture {
+                        circleColor = randomColor()
+                    }
+                    .padding()
+
+                Spacer()
+            }
+
+            Spacer()
+
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Welcome Alexandra!")
+            }
+
+            Spacer()
         }
         .foregroundColor(.orange)
-        .padding()
     }
 }
 
